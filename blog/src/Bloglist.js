d2c8e8603112ae413e508d0723b5e51e0c66b116
Satya <satyanaidugandrothu@gmail.com>
2023-05-29 12:36:44 +0530

    blog commit

---
+++ blog/src/Bloglist.js
@@ -0,0 +1,18 @@
+const Bloglist=(props)=>{
+   
+     let blogs=props.Blogs;
+    return (
+        <div>
+           {blogs.map((blog)=>{
+            return(
+           <div key={blog.id}>
+            <h3>Title:{blog.title}</h3>
+            <p>{blog.body}</p>
+            <h6>Written by:{blog.author}</h6>
+            </div>)
+          })} 
+       </div>
+    );
+    };
+    
+    export default Bloglist;
